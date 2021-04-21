052cb167d3994f9dd74dbf2b06c73f6efc750c66
IvanDushenko <ivandushenko@mail.ru>
2021-04-22 01:03:11 +0300

    Deleted needless file, add docstring

--- DecoratorHelper.py
+++ DecoratorHelper.py
@@ -1,4 +1,6 @@
 class DecoratorHelper:
+    """This class is intended for decorating called objects.
+    Adds attributes that store arguments to an object and returns it."""
 
     def __init__(self, *args, **kwargs):
         self.function = None
